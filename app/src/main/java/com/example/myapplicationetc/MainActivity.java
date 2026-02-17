3530deb3d5cc239a612f0a5092994153d0821da4
n0tcsbet <n0tcsbet.work@gmail.com>
2026-02-17 21:36:25 +0300

    День 1: Приложение с двумя экранами.

---
+++ app/src/main/java/com/example/myapplicationetc/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplicationetc;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonProfile = findViewById(R.id.buttonProfile);
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
